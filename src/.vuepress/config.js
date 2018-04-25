d831d310f93eded571bb8d14f284721c34010569
2vg <m0fqn090310@gmail.com>
2018-04-26 07:50:56 +0900

    add: add new nim-world files

---
+++ src/.vuepress/config.js
@@ -0,0 +1,25 @@
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    sidebar: [
+      ['/', 'Introduction'],
+      ['/chapter1', '1: Dive To Nim 👑'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter1-1', '1-1: こんにちは！Nimの世界！']
+        ]
+      },
+      ['/chapter2', '2: 様々な型'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter2-1', '2-1: 序数型'],
+          ['/chapter2-2', '2-2: 浮動小数点型'],
+          ['/chapter2-3', '2-3: 文字列型'],
+          ['/chapter2-4', '2-4: 構造化型'],
+        ]
+      },
+    ],
+  }
+}
---
+++ src/chapter1.md
@@ -0,0 +1,16 @@
+# Nimの世界へようこそ！
+
+Nimをよく知るには実際にコードを書いてみるのが一番です！まずはコンパイラを入手しましょう！
+
+公式サイトのInstallページから入手することが出来ます。[Install](https://nim-lang.org/install.html)
+
+Windowsユーザーの方はMinGWと各種DLLが必要です。
+
+MinGWは公式で配布されているこちらから、[MinGW 32bit](https://nim-lang.org/download/mingw32-6.3.0.7z)/[MinGW 64bit](https://nim-lang.org/download/mingw64-6.3.0.7z)
+
+DLLはこちらで入手できます。[DLL](https://nim-lang.org/download/dlls.zip)
+
+Unix系OSやmacOSの方はあらかじめCコンパイラ\(clang, gcc\)、そしてPCREとOpenSSLが必要です。ない場合はインストールしておきましょう。
+
+コンパイラをダウンロードしてビルドした後は必ず環境変数へパスの追加をしておきましょう。
+
