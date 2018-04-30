5cea7ac2b317c629df0437979aa1f63cb4357c00
rigani <rigani.exe@gmail.com>
2018-04-30 16:05:03 +0900

    amend comment

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -28,8 +28,7 @@ module.exports = {
       ...sidebarGroup('divetonim', [
         'hellonim'
       ]),
-      ...sidebarGroup('variable', [
-      ]),
+      ...sidebarGroup('variable'),
       ...sidebarGroup('types', [
         'ordinal',
         'floating',
@@ -46,29 +45,23 @@ module.exports = {
         'for',
         'iterator'
       ]),
-      // ['/chapter6', '6: 関数'],
-      // ['/chapter7', '7: 参照'],
-      // ['/chapter8', '8: テスト'],
-      // ['/chapter9', '9: モジュールとパッケージ'],
-      // {
-      //   collapsable: false,
-      //   children: [
-      //     ['/chapter9-1', '9-1: モジュール'],
-      //     ['/chapter9-2', '9-2: パッケージ']
-      //   ]
-      // },
-      // ['/chapter10', '10: 高度なNim'],
-      // {
-      //   collapsable: false,
-      //   children: [
-      //     ['/chapter10-1', '10-1: マクロ'],
-      //     ['/chapter10-2', '10-2: ポインタとメモリ'],
-      //     ['/chapter10-3', '10-3: マルチスレッド'],
-      //     ['/chapter10-4', '10-4: FFI'],
-      //     ['/chapter10-5', '10-5: コンパイルオプション'],
-      //     ['/chapter10-6', '10-6: アセンブリ']
-      //   ]
-      // },
+      // ...sidebarGroup('chapter6'), // 関数
+      // ...sidebarGroup('chapter7'), // 参照
+      // ...sidebarGroup('chapter8'), // テスト
+      // // モジュールとパッケージ
+      // ...sidebarGroup('chapter9', [
+      //   'chapter9-1', // モジュール
+      //   'chapter9-2', // パッケージ
+      // ]),
+      // // 高度なNim
+      // ...sidebarGroup('chapter10', [
+      //   'chapter10-1', // マクロ
+      //   'chapter10-2', // ポインタとメモリ
+      //   'chapter10-3', // マルチスレッド
+      //   'chapter10-4', // FFI
+      //   'chapter10-5', // コンパイルオプション
+      //   'chapter10-6', // アセンブリ
+      // ]),
     ],
   },
   markdown: {
