f74f272460c38ad0651935176953fbaf46682d1c
2vg <m0fqn090310@gmail.com>
2018-04-30 09:49:04 +0900

    add all navigation

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,14 +11,55 @@ module.exports = {
           ['/chapter1-1', '1-1: こんにちは！Nimの世界！']
         ]
       },
-      ['/chapter2', '2: 様々な型'],
+      ['/chapter2', '2: 変数'],
+      ['/chapter3', '3: 様々な型'],
       {
         collapsable: false,
         children: [
-          ['/chapter2-1', '2-1: 序数型'],
-          ['/chapter2-2', '2-2: 浮動小数点型'],
-          ['/chapter2-3', '2-3: 文字列型'],
-          ['/chapter2-4', '2-4: 構造化型'],
+          ['/chapter3-1', '3-1: 序数型'],
+          ['/chapter3-2', '3-2: 浮動小数点型'],
+          ['/chapter3-3', '3-3: 文字列型'],
+          ['/chapter3-4', '3-4: 構造化型']
+        ]
+      },
+      ['/chapter4', '4: 条件'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter4-1', '4-1: if'],
+          ['/chapter4-2', '4-2: case']
+        ]
+      },
+      ['/chapter5', '5: ループ'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter5-1', '5-1: while'],
+          ['/chapter5-2', '5-2: for'],
+          ['/chapter5-3', '5-3: iterator']
+        ]
+      },
+      ['/chapter6', '6: 関数'],
+      ['/chapter7', '7: 参照'],
+      ['/chapter8', '8: テスト'],
+      ['/chapter9', '9: モジュールとパッケージ'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter9-1', '9-1: モジュール'],
+          ['/chapter9-2', '9-2: パッケージ']
+        ]
+      },
+      ['/chapter10', '10: 高度なNim'],
+      {
+        collapsable: false,
+        children: [
+          ['/chapter10-1', '10-1: マクロ'],
+          ['/chapter10-2', '10-2: ポインタとメモリ'],
+          ['/chapter10-3', '10-3: マルチスレッド'],
+          ['/chapter10-4', '10-4: FFI'],
+          ['/chapter10-5', '10-5: コンパイルオプション'],
+          ['/chapter10-6', '10-6: アセンブリ']
         ]
       },
     ],
