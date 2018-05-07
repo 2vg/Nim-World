ce185c73429540ee0d7156d07af5013fc5b587e1
2vg <m0fqn090310@gmail.com>
2018-05-07 21:55:52 +0900

    disable collapsable

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,74 +2,74 @@
 const path = require('path')
 const twemoji = require('twemoji')
 
-const sidebarGroup = (dirName, children = [], collapsable = true) => {
-  let directory = path.join('/', dirName, '/')
-  return [
-    directory,
-    {
-      collapsable,
-      children: children.map(child => path.join(directory, child))
-    }
-  ]
+const sidebarGroup = (dirName, children = [], collapsable = false) => {
+    let directory = path.join('/', dirName, '/')
+    return [
+        directory,
+        {
+            collapsable,
+            children: children.map(child => path.join(directory, child))
+        }
+    ]
 }
 
 module.exports = {
-  base: '/',
-  locales: {
-    '/': {
-      lang: 'ja-JP',
-      title: 'NimWorld',
-      description: 'Nim言語の日本語解説文書'
+    base: '/',
+    locales: {
+        '/': {
+            lang: 'ja-JP',
+            title: 'NimWorld',
+            description: 'Nim言語の日本語解説文書'
+        }
+    },
+    themeConfig: {
+        sidebar: [
+            '/',
+            ...sidebarGroup('divetonim', [
+                'hellonim'
+            ]),
+            ...sidebarGroup('variable'),
+            ...sidebarGroup('types', [
+                'ordinal',
+                'floating',
+                'string',
+                'structured',
+                'generics'
+            ]),
+            ...sidebarGroup('condition', [
+                'if',
+                'case',
+                'when'
+            ]),
+            ...sidebarGroup('loop', [
+                'while',
+                'for',
+                'iterator'
+            ]),
+            ...sidebarGroup('proc'),
+            ...sidebarGroup('ref'),
+            ...sidebarGroup('tests'),
+            ...sidebarGroup('modules'),
+            ...sidebarGroup('packages'),
+            ...sidebarGroup('advancednim', [
+                'macros', // マクロ
+                　'ptrmem', // ポインタとメモリ
+                'multithreads', // マルチスレッド
+                'ffi', // FFI
+                'compileoptions', // コンパイルオプション
+                'nimscript', //NimScript
+                'asm' // アセンブリ
+            ]),
+        ],
+    },
+    markdown: {
+        config: md => {
+            md.renderer.rules.emoji = (token, idx) => {
+                return twemoji.parse(token[idx].content, {
+                    folder: 'svg',
+                    ext: '.svg'
+                })
+            }
+        }
     }
-  },
-  themeConfig: {
-    sidebar: [
-      '/',
-      ...sidebarGroup('divetonim', [
-        'hellonim'
-      ]),
-      ...sidebarGroup('variable'),
-      ...sidebarGroup('types', [
-        'ordinal',
-        'floating',
-        'string',
-        'structured',
-        'generics'
-      ]),
-      ...sidebarGroup('condition', [
-        'if',
-        'case',
-        'when'
-      ]),
-      ...sidebarGroup('loop', [
-        'while',
-        'for',
-        'iterator'
-      ]),
-      ...sidebarGroup('proc'),
-      ...sidebarGroup('ref'),
-      ...sidebarGroup('tests'),
-      ...sidebarGroup('modules'),
-      ...sidebarGroup('packages'),
-      ...sidebarGroup('advancednim', [
-        'macros', // マクロ
-      　 'ptrmem', // ポインタとメモリ
-        'multithreads', // マルチスレッド
-        'ffi', // FFI
-        'compileoptions', // コンパイルオプション
-        'nimscript', //NimScript
-        'asm' // アセンブリ
-      ]),
-    ],
-  },
-  markdown: {
-    config: md => {
-      md.renderer.rules.emoji = (token, idx) => {
-        return twemoji.parse(token[idx].content, {
-          folder: 'svg',
-          ext: '.svg'
-        })
-      }
-    }
-  }
 }
